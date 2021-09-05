05840527c2c1fcb1268daf80f2962a08f6504522
Artem <artem.marshalkin@gmail.com>
2021-09-05 11:56:48 +0300

    fix edx_course_student_notes_notes_page_viewed
    fix object_extensions_extra_fields for edx_course_student_notes_notes_page_viewed

--- openedx_caliper_tracking/transformers/navigation_transformers.py
+++ openedx_caliper_tracking/transformers/navigation_transformers.py
@@ -243,9 +243,11 @@ def edx_course_student_notes_notes_page_viewed(current_event, caliper_event):
         'extensions': json.loads(current_event['event'])
     }
 
-    caliper_object['extensions']['extra_fields'].update({
-        'course_id': current_event['context']['course_id'],
-        'org_id': caliper_event['extensions']['extra_fields'].pop('org_id')
+    caliper_object['extensions'].update({
+       'extra_fields': {
+          'course_id': current_event['context']['course_id'],
+          'org_id': caliper_event['extensions']['extra_fields'].pop('org_id')
+       }
     })
 
     caliper_event.update({
